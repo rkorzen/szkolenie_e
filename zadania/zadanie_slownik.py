8f78853525b95e6ffeeaaa122517b763b6cbd6cc
rkorzen <korzeniewski@gmail.com>
2024-03-19 09:28:36 +0100

    poczatek dzien 2

---
+++ zadania/zadanie_slownik.py
@@ -0,0 +1,21 @@
+"""
+
+Mamy tekst: Ala ma kota
+
+napisz skrypt, który wypisze słownik zawierajacy zliczenia znaków
+
+{
+"A": 1,
+"l": 1,
+"a": 3,
+...
+}
+
+klucz in slownik
+slownik[klucz] = slownik[klucz] + innawartosc
+
+for znak in tekst:
+   ...
+
+https://github.com/rkorzen/szkolenie_e   
+"""
