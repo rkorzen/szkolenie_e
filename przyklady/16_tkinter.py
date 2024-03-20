051bcb9e3cb8417df89497bd315a92a7dde83960
rkorzen <korzeniewski@gmail.com>
2024-03-20 10:34:09 +0100

    tkinter

--- przyklady/16_tkinter.py
+++ przyklady/16_tkinter.py
@@ -13,3 +13,29 @@ def get_currency_rate(currency_code):
                 return waluta["mid"]
     except requests.exceptions.RequestException as e:
         return f"Error: {e}"
+
+
+def on_submit():
+    currency_code = currency_combobox.get()
+    currency_rate = get_currency_rate(currency_code)
+    result_label.config(text=f"Kurs {currency_code} to {currency_rate}")
+
+app = tk.Tk()
+app.geometry("400x400")
+app.title("Kursy walut")
+
+tk.Label(app, text="Wybierz walutę", font=("Arial", 20)).pack()
+currency_combobox = ttk.Combobox(app, values=["USD", "EUR",])
+currency_combobox.pack()
+
+submit_button = tk.Button(
+    app,
+    text="Pobierz kurs",
+    font=("Arial", 20),
+    command=on_submit
+)
+submit_button.pack()
+result_label = tk.Label(app, text="tutaj będą kursy", font=("Arial", 20))
+result_label.pack()
+
+app.mainloop()
